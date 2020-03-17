75a9e225c2b56d54445d9879fde3336b18f5a4f2
Thomas Moon <mail@thomasmoon.net>
2020-03-18 00:26:58 +0200

    Improvements to employee list view.

--- components/payday/PaydayList.tsx
+++ components/payday/PaydayList.tsx
@@ -4,6 +4,7 @@ import { Employee } from '../../lib/payrollCalc/index';
 
 // Shared style
 import sharedStyle from '../../sharedStyles';
+import { faBold } from '@fortawesome/free-solid-svg-icons';
 
 export default function PaydayList (props: any) {
 
@@ -16,22 +17,42 @@ export default function PaydayList (props: any) {
           marginHorizontal: 24,
           marginBottom: 32,
           height: 300
-        }
+        },
+        item: {
+          paddingHorizontal: 16,
+          paddingVertical: 8,
+          borderBottomWidth: 1,
+          borderBottomColor: '#ccc'
+        },
+        title: {
+          fontSize: 24
+        },
     }
 
-    const itemPressed = () => {
-        Alert.alert('hello');
+    const Item = ({name}:any) => {
+      return (
+        <TouchableOpacity
+          style={styles.item}
+          onPress={() => { itemPressed(name)}}>
+          <Text style={styles.title}>{name}</Text>
+        </TouchableOpacity>
+      );
+    }
+
+    const itemPressed = (name: any) => {
+        Alert.alert('Clicked employee: ', name);
     }
 
     const [employees, setEmployees] = useState([
-        { name: 'Thomas', key: '1'}
+        { key: '1', name: 'Employee 1' },
+        { key: '2', name: 'Employee 2' },
+        { key: '3', name: 'Employee 3' },
+        { key: '4', name: 'Employee 4' }
     ])
 
     // Export the payroll info as text
   const exportList = async () => {
     try {
-
-      //nextPhase();
 
       let shareOptions = {
         message: ''
@@ -55,16 +76,12 @@ export default function PaydayList (props: any) {
 
     return (
       <View style={styles.activeScreen}>
-        <Text style={styles.helpText}>Payroll list</Text>
+        <Text style={styles.helpText}>Employees</Text>
         <FlatList
-          data={employees}
-          style={styles.payrollList}
-          renderItem={({ item: any }) => (
-            <TouchableOpacity onPress={itemPressed}>
-                <Text>Test</Text>
-            </TouchableOpacity>
-          )}
-        ></FlatList>
+        data={employees}
+        style={styles.payrollList}
+        renderItem={({ item }) => <Item name={item.name} />}
+      />
         <TouchableOpacity
           style={styles.whiteButton}
           onPress={exportList}>
